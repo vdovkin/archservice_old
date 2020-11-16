098652e2309aaa5b67f7794c0df07a632e85e47c
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2020-11-16 23:19:47 +0200

    big modifications. Update results page

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("form");
+const checkBox = document.getElementById("customgrid");
+const lenghtblock = document.getElementById("lenghtblock");
+const stepblock = document.getElementById("stepblock");
+const grid = document.getElementById("grid");
+const lenght = document.getElementById("lenght");
+
+function showGridSelection() {
+  // If the checkbox is checked, display
+  if (checkBox.checked == true) {
+    lenghtblock.style.display = "block";
+    stepblock.style.display = "block";
+    grid.style.display = "none";
+  } else {
+    lenghtblock.style.display = "none";
+    stepblock.style.display = "none";
+    grid.style.display = "block";
+  }
+}
+
+function saveValues() {
+  if (checkBox.checked == true) {
+    localStorage.setItem("length", lenght.value);
+  }
+}
